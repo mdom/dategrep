5f021cd469caddd573eab43dc26ab355f4c8c2b9
Mario Domgoergen <mario@domgoergen.com>
2014-10-17 22:48:23 +0200

    fixed typo

--- dategrep.el
+++ dategrep.el
@@ -1,7 +1,7 @@
 (defun dategrep-narrow (format start end)
-  (interactive "sFormar: \nsStart: \nsEnd: ")
   (with-temp-buffer
     (call-process "dategrep" nil t nil "--format" format "--start" start "--end" end (buffer-file-name))
     (message "%s" (buffer-string))))
+  (interactive "sFormat: \nsStart: \nsEnd: ")
 
 (provide 'dategrep)
