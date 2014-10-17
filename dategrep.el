d9f90e102e663870bf11df861921af4821ad9ece
Mario Domgoergen <mario@domgoergen.com>
2014-10-17 22:48:40 +0200

    implemented narrowing

--- dategrep.el
+++ dategrep.el
@@ -1,7 +1,12 @@
 (defun dategrep-narrow (format start end)
-  (with-temp-buffer
-    (call-process "dategrep" nil t nil "--format" format "--start" start "--end" end (buffer-file-name))
-    (message "%s" (buffer-string))))
   (interactive "sFormat: \nsStart: \nsEnd: ")
+  (let* ((file (buffer-file-name))
+         (offsets
+	  (with-temp-buffer
+	    (call-process "dategrep" nil t nil "--byte-offsets" "--format" format "--start" start "--end" end file)
+	    (mapcar 'string-to-number (split-string (buffer-string))))))   
+    (narrow-to-region
+     (byte-to-position (1+ (car  offsets)))
+     (byte-to-position (1+ (cadr offsets))))))
 
 (provide 'dategrep)
