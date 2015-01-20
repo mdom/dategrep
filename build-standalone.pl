96d782801037020ab200148fb78d6a318f37186c
Mario Domgoergen <mario@domgoergen.com>
2015-01-20 23:02:03 +0100

    automatically update gh-pages

--- build-standalone.pl
+++ build-standalone.pl
@@ -4,3 +4,7 @@ PERL5LIB=./lib:$PERL5LIB
 
 fatten --overwrite --quiet --exclude-dist=Date-Manip -o dategrep-standalone-small bin/dategrep
 fatten --overwrite --quiet --include-dist=Date-Manip -o dategrep-standalone-big   bin/dategrep
+
+git checkout gh-pages
+mv dategrep-standalone-small dategrep-standalone-small.pl
+mv dategrep-standalone-big   dategrep-standalone-big.pl
