25a0a220ca0a3fd51ffc39de303b2f03b767a807
Mario Domgoergen <mdom@taz.de>
2015-05-16 23:00:04 +0200

    add script to do github releases (closes #6)

--- github-release.sh
+++ github-release.sh
@@ -6,6 +6,8 @@ PERL5LIB=./lib:$PERL5LIB
 
 last_tag=$(git describe --abbrev=0 --tags)
 
+github-release release --user mdom --repo dategrep --tag $last_tag
+
 fatten --overwrite --quiet --exclude-dist=Date-Manip -o dategrep-standalone-small bin/dategrep
 fatten --overwrite --quiet --include-dist=Date-Manip -o dategrep-standalone-big   bin/dategrep
 
