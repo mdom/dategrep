8cc44f7136d3ac10ac23625d589df0e7e45e1496
Mario Domgoergen <mdom@taz.de>
2016-04-06 12:56:30 +0200

    Include core modules for standalone-big (fix #26)

--- github-release.sh
+++ github-release.sh
@@ -12,8 +12,8 @@ _fatten () {
 	fatten --overwrite --quiet --strip --exclude-dist=Class-XSAccessor "$@" bin/dategrep
 }
 
-_fatten --exclude-dist=Date-Manip -o dategrep-standalone-small
-_fatten --include-dist=Date-Manip -o dategrep-standalone-big
+_fatten --exclude-dist=Date-Manip                   -o dategrep-standalone-small
+_fatten --include-dist=Date-Manip --no-exclude-core -o dategrep-standalone-big
 
 github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-small --file dategrep-standalone-small
 github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-big   --file dategrep-standalone-big
