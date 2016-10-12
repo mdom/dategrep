f046153e4b2934b59b22a7129c4ea4cdb750c1a5
Mario Domgoergen <mdom@taz.de>
2016-10-12 13:24:18 +0200

    Added missing dist to build process

--- github-release.sh
+++ github-release.sh
@@ -6,15 +6,15 @@ PERL5LIB=./lib:$PERL5LIB
 
 last_tag=$(git describe --abbrev=0 --tags)
 
-github-release release --user mdom --repo dategrep --tag $last_tag
+#github-release release --user mdom --repo dategrep --tag $last_tag
 
 _fatten () {
-	depak --overwrite --quiet --stripper --exclude-dist=Class-XSAccessor "$@" bin/dategrep
+	depak --overwrite --quiet --stripper --include-dist=Method::Generate::BuildAll --exclude-dist=Class-XSAccessor "$@" bin/dategrep
 }
 
-_fatten --exclude-dist=Date-Manip                   -o dategrep-standalone-small
-_fatten --include-dist=Date-Manip --no-exclude-core -o dategrep-standalone-big
+_fatten --exclude-dist=Date-Manip -o dategrep-standalone-small
+_fatten --include-dist=Date-Manip -o dategrep-standalone-big
 
-github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-small --file dategrep-standalone-small
-github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-big   --file dategrep-standalone-big
+#github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-small --file dategrep-standalone-small
+#github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-big   --file dategrep-standalone-big
 
