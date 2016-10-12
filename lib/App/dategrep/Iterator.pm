9d2a0980c0563b04bdd61e1dfbc5fe21072284e4
Mario Domgoergen <mdom@taz.de>
2016-10-12 09:05:52 +0200

    Move general seek to FH.pm

--- lib/App/dategrep/Iterator.pm
+++ lib/App/dategrep/Iterator.pm
@@ -66,39 +66,7 @@ sub print {
 }
 
 sub BUILD {
-	shift->seek;
-}
-
-sub seek {
-    my $self = shift;
-    my $ignore = $self->multiline || $self->skip_unparsable;
-    while (1) {
-        my $line = $self->fh->getline;
-        if ( !$line ) {
-            $self->eof(1);
-            return;
-        }
-        my ( $date, $error ) = $self->to_epoch($line);
-
-        if ( !$date && $ignore ) {
-            next;
-        }
-        elsif ( !$date ) {
-            die "No date found in line $line";
-        }
-        elsif ( $date < $self->start ) {
-            next;
-        }
-        elsif ( $date >= $self->start && $date < $self->end ) {
-            $self->next_line($line);
-            $self->next_date($date);
-            return;
-        }
-        else {
-            $self->eof(1);
-            return;
-        }
-    }
+    shift->seek;
 }
 
 sub to_epoch {
--- lib/App/dategrep/Iterator/Fh.pm
+++ lib/App/dategrep/Iterator/Fh.pm
@@ -5,4 +5,36 @@ use Moo;
 use FileHandle;
 extends 'App::dategrep::Iterator';
 
+sub seek {
+    my $self = shift;
+    my $ignore = $self->multiline || $self->skip_unparsable;
+    while (1) {
+        my $line = $self->fh->getline;
+        if ( !$line ) {
+            $self->eof(1);
+            return;
+        }
+        my ( $date, $error ) = $self->to_epoch($line);
+
+        if ( !$date && $ignore ) {
+            next;
+        }
+        elsif ( !$date ) {
+            die "No date found in line $line";
+        }
+        elsif ( $date < $self->start ) {
+            next;
+        }
+        elsif ( $date >= $self->start && $date < $self->end ) {
+            $self->next_line($line);
+            $self->next_date($date);
+            return;
+        }
+        else {
+            $self->eof(1);
+            return;
+        }
+    }
+}
+
 1;
