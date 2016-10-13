ef432175ece8191e0f88d5dd05d7ab87a594608b
Mario Domgoergen <mdom@taz.de>
2016-10-13 11:03:52 +0200

    Build debian package for release (fix #18)

--- github-release.sh
+++ github-release.sh
@@ -6,7 +6,7 @@ PERL5LIB=./lib:$PERL5LIB
 
 last_tag=$(git describe --abbrev=0 --tags)
 
-#github-release release --user mdom --repo dategrep --tag $last_tag
+github-release release --user mdom --repo dategrep --tag $last_tag
 
 _fatten () {
 	depak --overwrite --quiet --stripper --include-dist=Method::Generate::BuildAll --exclude-dist=Class-XSAccessor "$@" bin/dategrep
@@ -15,6 +15,16 @@ _fatten () {
 _fatten --exclude-dist=Date-Manip -o dategrep-standalone-small
 _fatten --include-dist=Date-Manip -o dategrep-standalone-big
 
-#github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-small --file dategrep-standalone-small
-#github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-big   --file dategrep-standalone-big
 
+github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-small --file dategrep-standalone-small
+github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep-standalone-big   --file dategrep-standalone-big
+
+export EMAIL="mario@domgoergen.com"
+export NAME="Mario Domgoergen"
+
+dch -v ${last_tag#v} -u low
+dch -r
+
+sbuild -d stable
+
+github-release upload --user mdom --repo dategrep --tag $last_tag --name dategrep_${last_tag#v}-1_all.deb --file ../dategrep_${last_tag#v}-1_all.deb
